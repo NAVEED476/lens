2d75a6c401fe9f4323c0647b706a7aa72546ff4f
Naveed Shaik <shaiknaveed435@gmail.com>
2024-05-10 18:39:16 +0530

    viosion of lens component created

--- src/components/Vision.jsx
+++ src/components/Vision.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./vision.css";
+import { RiGroupFill } from "react-icons/ri";
 
 const Vision = () => {
   return (
@@ -13,6 +14,63 @@ const Vision = () => {
           research that refers to strategies that try to provide transparency to
           typically opaque AI models and their predictions.
         </p>
+      </div>
+      <div className="devider">
+        <div>
+          <div className="vision-role">
+            <h3 className="q-title">Diversity & Fairness</h3>
+            <span className="subtitle">
+              When training our AI models, we consider ethical and social
+              implications of algorithm-based decision making. Our solutions use
+              high-quality and fairly-represented data sets to eliminate human
+              cognitive biases.
+            </span>
+          </div>
+          <div>
+            <span>
+              <RiGroupFill />
+            </span>
+            <span className="dotted-line"></span>
+          </div>
+        </div>
+
+        <div>
+          <div className="vision-role">
+            <h3 className="q-title">Regulatory Compliance</h3>
+            <span className="subtitle">
+              Working with clients globally, LENS acknowledges applicable data
+              privacy regulations, such as the GDPR, HIPAA and others, in all
+              our solutions handling sensitive data. We also ensure our
+              algorithms allow for the required level of decision-making
+              transparency and explainability.
+            </span>
+          </div>
+          <div>
+            <span>
+              <RiGroupFill />
+            </span>
+            <span className="dotted-line"></span>
+          </div>
+        </div>
+
+        <div>
+          <div className="vision-role">
+            <h3 className="q-title">Code of Ethics</h3>
+            <span className="subtitle">
+              LENS believes in a just, non-violent world of equality and
+              fairness. We prize democratic values, civil liberties and open and
+              informed debate. When used to further these values,
+              algorithm-based decision-making models can continue to make the
+              world a safer, better place for everyone.
+            </span>
+          </div>
+          <div>
+            <span>
+              <RiGroupFill />
+            </span>
+            <span className="dotted-line"></span>
+          </div>
+        </div>
       </div>
     </div>
   );
