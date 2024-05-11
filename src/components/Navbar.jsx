b1bb6d3a053326f40cfa66966d31563f0dc258a0
Naveed Shaik <shaiknaveed435@gmail.com>
2024-05-11 10:03:28 +0530

    removed unncessary commented code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,3 @@
-// import React from 'react';
-// import './navbar.css';
-
-// const Navbar = () => {
-//   return (
-//     <header className="header">
-//       <img src="https://lenscorp.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fnav_logo.e5fb945a.png&w=96&q=75" alt="Logo" className="logo" />
-//       <nav>
-//         <ul className="nav-links">
-//           <li href="#">Home</li>
-//           <li href="#">Company</li>
-//           <li href="#">Blogs</li>
-//         </ul>
-//       </nav>
-//     </header>
-//   );
-// };
-
-// export default Navbar;
 
 import React from "react";
 import { MdDarkMode } from "react-icons/md";
